fe0d58f17759950d2f614028a7dac4afb9323e90
Denis Migranov <d.migranov@g.nsu.ru>
2019-09-09 19:45:24 +0700

    first

---
+++ ModuloPower/CryptoTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace ModuloPower
+{
+    class CryptoTools
+    {
+        //System.Numerics.BigInteger ModPow
+        public static BigInteger ModuloPower()
+        {
+
+        }
+    }
+}
