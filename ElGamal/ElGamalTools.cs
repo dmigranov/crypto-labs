3644d3ddedff567f630d85b8386c79aade3d171a
Denis Migranov <d.migranov@g.nsu.ru>
2019-09-30 19:30:52 +0700

    priv

--- ElGamal/ElGamalTools.cs
+++ ElGamal/ElGamalTools.cs
@@ -5,7 +5,7 @@ namespace Crypto
 {
     class ElGamalTools
     {
-        public static BigInteger GenerateElGamalPrivateKey(BigInteger g, BigInteger p)
+        private static BigInteger GenerateElGamalPrivateKey(BigInteger g, BigInteger p)
         {
             Random r = new Random();
             int s = r.Next(0, (int)p);
@@ -13,12 +13,12 @@ namespace Crypto
             return CryptoTools.ModuloPower(g, s, p);    //т.к s - это случайное число в диапазоне только от 0 до MAX_INT
         }
 
-        public static BigInteger GenerateElGamalPublicKey(BigInteger g, BigInteger privateKey, BigInteger p)
+        private static BigInteger GenerateElGamalPublicKey(BigInteger g, BigInteger privateKey, BigInteger p)
         {
             return CryptoTools.ModuloPower(g, privateKey, p);
         }
 
-        public static void EncryptMessage(BigInteger g, BigInteger receiverPublicKey, BigInteger p, BigInteger message, out BigInteger y, out BigInteger k)
+        private static void EncryptMessage(BigInteger g, BigInteger receiverPublicKey, BigInteger p, BigInteger message, out BigInteger y, out BigInteger k)
         {
             Random random = new Random();
             int s = random.Next(0, (int)p);
@@ -30,7 +30,7 @@ namespace Crypto
 
         }
 
-        public static BigInteger DecryptMessage(BigInteger receiverPrivateKey, BigInteger p, BigInteger y, BigInteger w)
+        private static BigInteger DecryptMessage(BigInteger receiverPrivateKey, BigInteger p, BigInteger y, BigInteger w)
         {
             return 0;
 
