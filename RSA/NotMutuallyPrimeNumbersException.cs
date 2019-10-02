b9813bd01b156957d8786dc29e3f5aaf640b3c07
Denis Migranov <d.migranov@g.nsu.ru>
2019-10-02 19:49:01 +0700

    RSA типа done но можно еще сделать генерацию c если поданныей не подойдет

---
+++ RSA/NotMutuallyPrimeNumbersException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Crypto
+{
+    [Serializable]
+    internal class NotMutuallyPrimeNumbersException : Exception
+    {
+        public NotMutuallyPrimeNumbersException()
+        {
+        }
+
+        public NotMutuallyPrimeNumbersException(string message) : base(message)
+        {
+        }
+
+        public NotMutuallyPrimeNumbersException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NotMutuallyPrimeNumbersException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- RSA/RSATools.cs
+++ RSA/RSATools.cs
@@ -5,9 +5,27 @@ namespace Crypto
 {
     internal class RSATools
     {
-        internal static void SimulateRSAExchange(BigInteger p, BigInteger q, BigInteger d, BigInteger m)
+        internal static void SimulateRSAExchange(BigInteger p, BigInteger q, BigInteger d, BigInteger x)
         {
-            throw new NotImplementedException();
+            BigInteger N = p * q;
+            BigInteger phi = (p - 1) * (q - 1);
+
+            BigInteger c, temp;
+            BigInteger ret = CryptoTools.EuclidAlgorithm(phi, d, out temp, out c);
+            if (ret != 1)
+                throw new NotMutuallyPrimeNumbersException();
+            if (c < 0)
+                c += phi;
+            //todo: генерация как в шамире если не получилось?
+            Console.WriteLine($"Receiver's public keys are N = {N} and d = {d}");
+            Console.WriteLine($"Receiver's private key is c = {c}");
+
+
+            BigInteger y = CryptoTools.ModuloPower(x, d, N);
+            Console.WriteLine($"Sender encrypted message and sent y = {y} to receiver");
+
+            BigInteger w = CryptoTools.ModuloPower(y, c, N);
+            Console.WriteLine($"Receiver decrypted y and got w = {w}");
         }
     }
 }
