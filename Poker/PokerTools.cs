8ee11018139b8a0d9ec31e7dcfd1199ca158201c
Denis Migranov <d.migranov@g.nsu.ru>
2019-10-05 19:07:52 +0700

    авава

--- Poker/PokerTools.cs
+++ Poker/PokerTools.cs
@@ -9,7 +9,28 @@ namespace Crypto
     {
         internal static void SimulatePokerExchange(BigInteger p)
         {
+            BigInteger cA, dA, cB, dB;
+            GeneratePokerPrivateKeys(p, out cA, out dA);
+            Console.WriteLine($"Alice's private keys are cA = {cA} and dA = {dA}");
+            GeneratePokerPrivateKeys(p, out cB, out dB);
+            Console.WriteLine($"Alice's private keys are cB = {cB} and dB = {dB}");
 
+            BigInteger alpha, beta, gamma; //карты
         }
+
+        private static void GeneratePokerPrivateKeys(BigInteger p, out BigInteger c, out BigInteger d)
+        {
+            BigInteger cCandidate, dCandidate, ret, temp;
+            do
+            {
+                cCandidate = CryptoTools.GenerateRandomBigInteger(1, p - 1);
+                ret = CryptoTools.EuclidAlgorithm(p - 1, cCandidate, out temp, out dCandidate);
+            } while (ret != 1); //крутимся пока не найдём такое cCandidate: (cCandidate, p - 1) = 1. тогда dCandidate обратное к cCandidate с точностью до модуля
+            c = cCandidate;
+            if (dCandidate < 0)
+                dCandidate += (p - 1);
+            d = dCandidate;
+        }
+
     }
 }
