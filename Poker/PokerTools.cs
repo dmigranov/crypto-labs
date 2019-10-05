5b4960216b83ef7bf42575e90ed2850633a83d17
Denis Migranov <d.migranov@g.nsu.ru>
2019-10-05 20:20:23 +0700

    triplet

--- Poker/PokerTools.cs
+++ Poker/PokerTools.cs
@@ -19,7 +19,28 @@ namespace Crypto
             GenerateCardNumbers(p, out alpha, out beta, out gamma);
             Console.WriteLine($"Alice generated numbers for cards and told Bob that alpha is {alpha}, beta is {beta}, gamma is {gamma}");
 
+            Console.WriteLine("START OF THE EXCHANGE");
+            Triplet cards = new Triplet(alpha, beta, gamma);
 
+        }
+
+        private struct Triplet
+        {
+            public BigInteger X { get; set; }
+            public BigInteger Y { get; set; }
+            public BigInteger Z { get; set; }
+            public Triplet(BigInteger x, BigInteger y, BigInteger z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+
+            public static Triplet ModuloPower(Triplet val, BigInteger exp, BigInteger mod)
+            {
+                return new Triplet(CryptoTools.ModuloPower(val.X, exp, mod), CryptoTools.ModuloPower(val.Y, exp, mod), CryptoTools.ModuloPower(val.Z, exp, mod));
+            }
+           
         }
 
         private static void GeneratePokerPrivateKeys(BigInteger p, out BigInteger c, out BigInteger d)
