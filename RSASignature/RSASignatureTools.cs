6cacfbc4f76169703f671c931b7db9f9b34a6134
Denis Migranov <d.migranov@g.nsu.ru>
2019-10-07 20:29:19 +0700

    en

---
+++ RSASignature/RSASignatureTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Crypto
+{
+    class RSASignatureTools
+    {
+        internal static void SimulateRSASigning(BigInteger p, BigInteger q, BigInteger d, BigInteger x)
+        {
+
+        }
+
+    }
+}
