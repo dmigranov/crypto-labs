cbc48e5087f2dcd1b629bf5afe6aef175df14a2c
Denis Migranov <d.migranov@g.nsu.ru>
2019-10-12 20:41:02 +0700

    found

--- BabyStepGiantStep/BSGSTools.cs
+++ BabyStepGiantStep/BSGSTools.cs
@@ -1,31 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Numerics;
 
 namespace Crypto
 {
 
 
-    internal class NumberIndex
+    internal class NumberIndex : IComparable<NumberIndex>
     {
 
         public BigInteger Number { get; set; }
-        public int Index { get; set; }
+        public BigInteger Index { get; set; }
+        public bool IsBaby { get; } //0 Baby 1 Giant
 
-        public Boolean IsBaby { get; } //0 Baby 1 Giant
+        public NumberIndex(BigInteger number, BigInteger index, bool isBaby)
+        {
+            Number = number;
+            Index = index;
+            IsBaby = isBaby;
+        }
 
 
-
-
+        public int CompareTo([AllowNull] NumberIndex other)
+        {
+            if (this.Number > other.Number)
+                return 1;
+            if (this.Number < other.Number)
+                return -1;
+            else
+                return 0;
+        }
     }
 
     internal class BSGSTools
     {
-        private static List<NumberIndex> babyList = new List<NumberIndex>(), giantList = new List<NumberIndex>();
+        //private static List<NumberIndex> babyList = new List<NumberIndex>(), giantList = new List<NumberIndex>();
 
         internal static void SolveEquation(BigInteger y, BigInteger a, BigInteger p)
         {
-            throw new NotImplementedException();
+            List<NumberIndex> commonList = new List<NumberIndex>();
+
+            BigInteger k, m;
+            k = m = (BigInteger)Math.Ceiling(Math.Sqrt((double)p));
+            Console.WriteLine("first");
+            for (BigInteger i = 0; i < m; i++)
+            {
+                NumberIndex n = new NumberIndex((y * CryptoTools.ModuloPower(a, i, p)) % p, i, true);
+                commonList.Add(n);
+                Console.WriteLine(n.Number);
+            }
+            Console.WriteLine("second");
+
+            for (BigInteger j = 1; j <= k; j++)
+            {
+                NumberIndex n = new NumberIndex(CryptoTools.ModuloPower(a, j * m, p), k, false);
+                commonList.Add(n);
+                Console.WriteLine(n.Number);
+            }
+
+            commonList.Sort();
+
+            NumberIndex prev = null, cur = null;
+            foreach(var element in commonList)
+            {
+                if(cur != null)
+                    prev = cur;
+                cur = element;
+
+                if (prev != null)
+                    if (prev.Number == cur.Number)
+                        Console.WriteLine($"{prev.Number} {cur.IsBaby} {prev.IsBaby} {cur.Index}");
+            }
+
         }
     }
 }
