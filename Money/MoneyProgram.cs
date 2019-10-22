976132d6633882776835bb74aa75bf3ecdd826fa
Denis Migranov <d.migranov@g.nsu.ru>
2019-10-22 20:41:52 +0700

    added money program

---
+++ Money/MoneyProgram.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Numerics;
+
+
+namespace Crypto
+{
+    class MoneyProgram
+    {
+        static void Main(string[] args)
+        {
+            BigInteger p;
+
+            p = AskForBigIntegerInput("Please enter prime number p, p > 0", x => x > 0);
+            Console.WriteLine($"p is {p}");
+
+        }
+
+
+        private static BigInteger AskForBigIntegerInput(string message, Predicate<BigInteger> cond)
+        {
+            BigInteger x;
+
+        Cycle:
+            Console.WriteLine(message);
+            string s = Console.ReadLine();
+            try
+            {
+                x = BigInteger.Parse(s);
+                if (!cond(x))
+                    goto Cycle;
+            }
+            catch (FormatException)
+            {
+                goto Cycle;
+            }
+
+            return x;
+        }
+    }
+
+
+
+
+}
