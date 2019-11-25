6b4fcbe188208505fa130664101667b406a47580
Denis Migranov <d.migranov@g.nsu.ru>
2019-11-25 19:48:15 +0700

    вывод ьинарного числа

--- OneTimePad/OneTimePadProgram.cs
+++ OneTimePad/OneTimePadProgram.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Numerics;
 using System.Threading.Tasks;
 
@@ -13,6 +14,9 @@ namespace Crypto
             do
             {
                 BigInteger m = AskForBigIntegerInput($"Please enter message, 0 <= m < 2^n = {BigInteger.Pow(2, n)}", x => (x >= 0 && x < BigInteger.Pow(2, n)));
+                byte[] a = m.ToByteArray();
+                string s = string.Join(" ", a.Reverse().Select(x => Convert.ToString(x, 2).PadLeft(8, '0')));
+                Console.WriteLine($"Your message is {s}");
                 OneTimePadTools.SendMessage(n, m);
             }
             while (true);
