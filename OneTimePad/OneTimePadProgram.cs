a2b39e7436c71411b988715ff172ddf03e050893
Denis Migranov <d.migranov@g.nsu.ru>
2019-11-25 17:57:41 +0700

    created one time pad project

---
+++ OneTimePad/OneTimePadProgram.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace OneTimePad
+{
+    static class OneTimePadProgram
+    {
+        
+        static void Main()
+        {
+            
+        }
+
+        private static BigInteger AskForBigIntegerInput(string message, Predicate<BigInteger> cond)
+        {
+            BigInteger x;
+
+        Cycle:
+            Console.WriteLine(message);
+            string s = Console.ReadLine();
+            try
+            {
+                x = BigInteger.Parse(s);
+                if (!cond(x))
+                    goto Cycle;
+            }
+            catch (FormatException)
+            {
+                goto Cycle;
+            }
+
+            return x;
+        }
+    }
+}
