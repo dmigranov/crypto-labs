9a0a3eb671ec834c4f69ea626b03885dde31c8b3
Denis Migranov <d.migranov@g.nsu.ru>
2019-11-25 20:25:30 +0700

    перенес в класс

--- OneTimePad/OneTimePadProgram.cs
+++ OneTimePad/OneTimePadProgram.cs
@@ -15,18 +15,8 @@ namespace Crypto
             do
             {
                 BigInteger m = AskForBigIntegerInput($"Please enter message, 0 <= m < 2^n = {BigInteger.Pow(2, n)}", x => (x >= 0 && x < BigInteger.Pow(2, n)));
-                /*byte[] a = m.ToByteArray(true, true);
-                string s = Convert.ToString(a[0], 2).PadLeft(n % 8 != 0 ? n % 8 : 8, '0');
-                for(int i = 1; i < (n % 8 == 0 ? n/8 : n/8 + 1); i++)
-                {
-                    if (i < a.Length)
-                        s += Convert.ToString(a[i], 2).PadLeft(8, '0');
 
-                }*/
-                string str = m.ToBinaryString();
-                str = str.PadLeft(n, '0');
-                Console.WriteLine($"Your message is {str}");
-                //OneTimePadTools.SendMessage(n, m);
+                OneTimePadTools.SendMessage(n, m);
             }
             while (true);
         }
@@ -74,35 +64,6 @@ namespace Crypto
         }
 
 
-        public static string ToBinaryString(this BigInteger bigint)
-        {
-            var bytes = bigint.ToByteArray(true, false);
-            var idx = bytes.Length - 1;
-
-            // Create a StringBuilder having appropriate capacity.
-            var base2 = new StringBuilder(bytes.Length * 8);
-
-            // Convert first byte to binary.
-            var binary = Convert.ToString(bytes[idx], 2);
-
-            // Ensure leading zero exists if value is positive.
-            /*if (binary[0] != '0' && bigint.Sign == 1)
-            {
-                base2.Append('0');
-            }*/
-
-
-
-            // Append binary string to StringBuilder.
-            base2.Append(binary);
-
-            // Convert remaining bytes adding leading zeros.
-            for (idx--; idx >= 0; idx--)
-            {
-                base2.Append(Convert.ToString(bytes[idx], 2).PadLeft(8, '0'));
-            }
-
-            return base2.ToString();
-        }
+        
     }
 }
--- OneTimePad/OneTimePadTools.cs
+++ OneTimePad/OneTimePadTools.cs
@@ -5,11 +5,15 @@ using System.Text;
 
 namespace Crypto
 {
-    class OneTimePadTools
+    static class OneTimePadTools
     {
 
         internal static void SendMessage(int n, BigInteger msg)
         {
+            string str = msg.ToBinaryString();
+            str = str.PadLeft(n, '0');
+            Console.WriteLine($"Your message is {str}");
+
             BigInteger k = GenerateKey(n);
             Console.WriteLine($"Secret key k = {k} generated");
 
@@ -17,7 +21,39 @@ namespace Crypto
 
         private static BigInteger GenerateKey(int n)
         {
-            throw new NotImplementedException();
+            return 0;
+        }
+
+
+        public static string ToBinaryString(this BigInteger bigint)
+        {
+            var bytes = bigint.ToByteArray(true, false);
+            var idx = bytes.Length - 1;
+
+            // Create a StringBuilder having appropriate capacity.
+            var base2 = new StringBuilder(bytes.Length * 8);
+
+            // Convert first byte to binary.
+            var binary = Convert.ToString(bytes[idx], 2);
+
+            // Ensure leading zero exists if value is positive.
+            /*if (binary[0] != '0' && bigint.Sign == 1)
+            {
+                base2.Append('0');
+            }*/
+
+
+
+            // Append binary string to StringBuilder.
+            base2.Append(binary);
+
+            // Convert remaining bytes adding leading zeros.
+            for (idx--; idx >= 0; idx--)
+            {
+                base2.Append(Convert.ToString(bytes[idx], 2).PadLeft(8, '0'));
+            }
+
+            return base2.ToString();
         }
     }
 }
